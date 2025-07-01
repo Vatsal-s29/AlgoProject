12e681e79491ade1d76cd46faf9f45e7564246c3
Vatsal-s29 <singhvatsal000@gmail.com>
2025-07-01 13:29:44 +0530

    removed back-button completely

--- frontend/src/pages/ShowQuestion.jsx
+++ frontend/src/pages/ShowQuestion.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 import getDifficultyClasses from "../utils/getDifficultyClasses";
 import { BACKEND_URL } from "../../config";
