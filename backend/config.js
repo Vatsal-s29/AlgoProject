2d5911919e2b4d21dd7f6c8ad13a31b685891446
Vatsal Singh <163160559+Vatsal-s29@users.noreply.github.com>
2025-08-03 00:55:11 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-    "mongodb+srv://root:root@books-store-mern.7joc4kd.mongodb.net/questions-collection?retryWrites=true&w=majority&appName=Books-Store-MERN";
+    "yoururl";
 
 
